2176b058cfede8e6bc63fae0a971f8a45731b53f
Antoine Lemoine <antoine.lemoine@gmail.com>
2016-10-24 16:55:06 +0200

    Remove useless empty setup class

--- app/code/community/Owebia/Shipping2/Model/Mysql4/Setup.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © 2008-2016 Owebia. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-class Owebia_Shipping2_Model_Mysql4_Setup extends Mage_Catalog_Model_Resource_Eav_Mysql4_Setup
-{
-}
